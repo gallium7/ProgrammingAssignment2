93fee77a5f84d373b641c013197e071f7d6979fd
Gallium7 <gary.schurr@gmail.com>
2014-08-21 13:02:36 +1000

    added functionality in R studio

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Determines the inverse of a mitrix using the solve() function; caches result
+## Firstly checks if cached result is available; if so, uses that and no need to re-calculate
 
-## Write a short comment describing this function
+## Creates an object of class list
+## Stores the inverse of the matrix calculated in cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function()  {
+                x
+        }
+        setinv <- function(inv) {
+                m <<- inv
+        }
+        getinv <- function() {
+                m
+        }
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
+## Determines if inverse of matrix has already been determined
+## - if not, this is calculated then sent to makeCacheMatrix
+## - if so it retrieves the cached matrix inverse
+## The inverse of the matrix is then returned
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(w, ...) {
+        ## Return a matrix that is the inverse of 'w'
+        m <- w$getinv()
+        if(!is.null(m)) {
+                message("getting cached data...")
+                return(m)
+        }
+        data <- w$get()
+        m <- solve(data, ...)
+        w$setinv(m)
+        m
 }
